093398dbe2a706cb3ddfcefeffbb22cb8a2befb3
bdileo35 <bdileo35@gmail.com>
2025-11-11 11:10:25 -0300

    Unificar conexiones Prisma - Usar singleton en todas las APIs

--- app/api/menu/[idUnico]/categories/route.ts
+++ app/api/menu/[idUnico]/categories/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '@/lib/prisma';
 
 // GET - Obtener categorías por idUnico
 export async function GET(
